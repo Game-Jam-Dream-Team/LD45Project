ddb5f080451380f6787c0002d46d3e395801cd6d
KonH <konh@yandex.ru>
2019-09-27 21:06:27 +0700

    Add build number view

---
+++ Assets/Scripts/BuildNumberView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class BuildNumberView : MonoBehaviour {
+	void Start() {
+		GetComponent<TMP_Text>().text = Application.version;
+	}
+}
