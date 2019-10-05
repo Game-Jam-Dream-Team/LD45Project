6f0d4d26ac2db765ed236ebcfb82084a83eb4c50
Kablaev@gmail.com <jetadore404>
2019-10-05 17:34:31 +0700

    added rotating obstacles

---
+++ Assets/Scripts/Obstacles/ObstacleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObstacleScript : MonoBehaviour
+{
+    float tilt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilt = Random.Range(-10, 10) / 5;
+        if (System.Math.Abs(tilt) < 0.2)
+        {
+            tilt = 0.2f;
+        }
+    }
+
+    void Update()
+    {
+        objectRotate();
+    }
+
+    void objectRotate()
+    {
+        transform.Rotate(Vector3.forward * tilt);
+    }
+}
