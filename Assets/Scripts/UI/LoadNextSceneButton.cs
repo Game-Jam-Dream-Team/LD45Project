a5b33b3fa79d1cbd9205b50b33db80bf35b31af6
KonH <konh@yandex.ru>
2019-10-05 10:20:44 +0700

    Load next scene button

---
+++ Assets/Scripts/UI/LoadNextSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class LoadNextSceneButton : MonoBehaviour {
+	void Start() {
+		GetComponent<Button>().onClick.AddListener(LoadNextScene);
+	}
+
+	void LoadNextScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
