ca51e9589fd984c59d13a7d2345911425dec271d
klimashin <klimashin.v@gmail.com>
2019-10-05 14:19:44 +0700

    Add result panel and scene controller

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController _instance;
+
+    public static SceneController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var go = new GameObject("SceneManager");
+                var manager = go.AddComponent<SceneController>();
+                DontDestroyOnLoad(go);
+                _instance = manager;
+            }
+
+            return _instance;
+        }
+    }
+
+    public void NextLevel()
+    {
+        var current = SceneManager.GetActiveScene().name;
+        var lvlNumber = int.Parse(Regex.Match("Level_12", @"(\d+)").Value);
+        lvlNumber++;
+
+        var sceneName = $"Level{lvlNumber}";
+        if (Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ReloadCurrent()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/SceneControllerInterface.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControllerInterface : MonoBehaviour
+{
+    private SceneController _sceneController;
+
+    private void Awake()
+    {
+        _sceneController = SceneController.Instance;
+    }
+
+    public void ToMenu()
+    {
+        _sceneController.ToMenu();
+    }
+
+    public void ReloadCurrent()
+    {
+        _sceneController.ReloadCurrent();
+    }
+
+    public void NextLevel()
+    {
+        _sceneController.NextLevel();
+    }
+}
