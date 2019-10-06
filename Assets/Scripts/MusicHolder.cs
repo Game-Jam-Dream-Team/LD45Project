e229ac0b051df9e17d3b607e3fcf88156f661143
KonH <konh@yandex.ru>
2019-10-06 13:41:29 +0700

    Add music

---
+++ Assets/Scripts/MusicHolder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicHolder: MonoBehaviour {
+	static MusicHolder _instance;
+
+	public float FadeInTime;
+
+	AudioSource _source;
+	float _wantedVolume;
+
+	float _timer;
+
+	void Awake() {
+		if ( _instance ) {
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+		_source = GetComponent<AudioSource>();
+		_source.outputAudioMixerGroup.audioMixer.GetFloat("MusicVolume", out _wantedVolume);
+		_instance = this;
+	}
+
+	void Update() {
+		_timer += Time.deltaTime * (1 / FadeInTime);
+		if ( _timer > 1.0f ) {
+			enabled = false;
+		}
+		var value = Mathf.Lerp(-80, _wantedVolume, _timer);
+		_source.outputAudioMixerGroup.audioMixer.SetFloat("MusicVolume", value);
+	}
+}
