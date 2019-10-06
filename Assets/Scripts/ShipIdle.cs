08130f809979caec9fe5ffb4fef1e19efa01024a
Kablaev@gmail.com <jetadore404>
2019-10-06 15:10:06 +0700

    fixed scenes for 1440x900

---
+++ Assets/Scripts/ShipIdle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipIdle : MonoBehaviour
+{
+    GameObject ship;
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   ship = GameObject.FindGameObjectWithTag("ship");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, 1, Space.Self);
+    }
+}
